f60caf13583e6724cfddc890e9cad59901a8b7a6
0leyyy <houdeknic@gmail.com>
2025-09-24 18:29:27 +0200

    gars deleted :sad:

--- src/main/java/net/mcreator/wmod/WModMod.java
+++ src/main/java/net/mcreator/wmod/WModMod.java
@@ -18,8 +18,6 @@ import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.FriendlyByteBuf;
 
-import net.mcreator.wmod.init.WModModItems;
-
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.Map;
 import java.util.List;
@@ -37,8 +35,6 @@ public class WModMod {
 		// End of user code block mod constructor
 		NeoForge.EVENT_BUS.register(this);
 		modEventBus.addListener(this::registerNetworking);
-
-		WModModItems.REGISTRY.register(modEventBus);
 
 		// Start of user code block mod init
 		// End of user code block mod init
--- src/main/java/net/mcreator/wmod/item/GarsItem.java
+++
@@ -1,7 +0,0 @@
-package net.mcreator.wmod.item;
-
-public class GarsItem extends Item {
-	public GarsItem() {
-		super(new Item.Properties());
-	}
-}
